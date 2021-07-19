57b2b07b2b230320b5afe158781c2d0ee655dad0
FortranVBA <mpyfabrice@gmail.com>
2021-07-19 11:16:02 +0200

    test réservation maximum 12 places

---
+++ test_server.py
@@ -0,0 +1,38 @@
+import pytest
+from flask import Flask, render_template, request
+
+import server
+
+@pytest.fixture
+def client(monkeypatch):
+    def mock_loadClubs():
+        return [
+            {
+                "name":"Simply Lift",
+                "email":"john@simplylift.co",
+                "points":"13"
+            },
+        ]
+    monkeypatch.setattr("server.loadClubs", mock_loadClubs, raising=True)
+
+    def mock_loadCompetitions():
+        return [
+            {
+                "name": "Spring Festival",
+                "date": "2100-03-27 10:00:00",
+                "numberOfPlaces": "25"
+            }            
+        ]
+    monkeypatch.setattr("server.loadCompetitions", mock_loadCompetitions, raising=True)
+
+    app = server.create_app()
+
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_display_point_balance(client):
+
+    response = client.get('/book/Spring%20Festival/Simply%20Lift')
+    assert b'<input type="number" name="places" id="" min="0" max=12 />' in response.data
+    
